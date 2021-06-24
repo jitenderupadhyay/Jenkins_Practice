db62bade5c32041654635d4c6ce0a2dde2e06c29
Jitender Upadhyay <j.upadhyay.niec@gmail.com>
2021-06-24 15:02:56 +0530

    Updated build failure

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,7 +38,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetIncludeGreetingMessage() {
     String someone = "World, Learning CI using Maven";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
